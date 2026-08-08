a326947548f1fd594e525cbe986762ff36f36c44
Mohammad Hossein <kavieswarten@gmail.com>
2026-08-08 11:34:02 +0330

    home page, database (mongoDB Atlas), fetching data from database, google auth setup, properties page, property detail page and few features added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import "@/assets/styles/globals.css";
 
 export const metadata = {
@@ -10,7 +12,9 @@ const MainLayout = ({ children }) => {
   return (
     <html>
       <body cz-shortcut-listen="true">
+        <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
