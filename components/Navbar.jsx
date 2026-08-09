5741e0cc1785abff16593647c0b2101dcc5d5ea1
Mohammad Hossein <kavieswarten@gmail.com>
2026-08-09 14:58:08 +0330

    sign out and protected route added so authentication finished, add property page styling, server action for submitting form, get form data added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -202,6 +202,10 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
                     >
                       Sign Out
                     </button>
