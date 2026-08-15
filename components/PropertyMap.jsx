5a6b2b6ff826d231c1728f987b5719054a31ed6f
Mohammad Hossein <kavieswarten@gmail.com>
2026-08-15 13:36:12 +0330

    map added to property detials, providers of map changed to locationiq, maptiler and maplibre

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -1,47 +1,37 @@
 "use client";
 import { useEffect, useState } from "react";
-import { setDefaults, fromAddress } from "react-geocode";
+import Map, { Marker } from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+import Image from "next/image";
+import pin from "@/assets/images/pin.svg";
+import Spinner from "./Spinner";
 
 const PropertyMap = ({ property }) => {
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
-  const [viewport, setViewport] = useState({
-    latitude: 0,
-    longitude: 0,
-    zoom: 12,
-    width: "100%",
-    height: "500px",
-  });
   const [loading, setLoading] = useState(true);
   const [geocodeError, setGeocodeError] = useState(false);
-
-  setDefaults({
-    key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
-    language: "en",
-    region: "us",
-  });
 
   useEffect(() => {
     const fetchCoords = async () => {
       try {
-        const res = await fromAddress(
-          `${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`,
+        const fullAddress = `${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`;
+        const response = await fetch(
+          `https://us1.locationiq.com/v1/search?key=${process.env.NEXT_PUBLIC_LOCATIONIQ_KEY}&q=${encodeURIComponent(fullAddress)}&format=json`,
         );
+        const res = await response.json();
 
         // Check geocode results
-        if (res.results.length === 0) {
+        if (res.length === 0) {
           setGeocodeError(true);
           return;
         }
 
-        const { lat, lng } = res.results[0].geometry.location;
+        const lat = Number(res[0].lat);
+        const lng = Number(res[0].lon);
+
         setLat(lat);
         setLng(lng);
-        setViewport({
-          ...viewport,
-          latitude: lat,
-          longitude: lng,
-        });
       } catch (error) {
         console.log(error);
         setGeocodeError(true);
@@ -53,12 +43,28 @@ const PropertyMap = ({ property }) => {
     fetchCoords();
   }, []);
 
-  if (loading) return <h3>Loading...</h3>;
+  if (loading) return <Spinner />;
 
   if (geocodeError)
     return <div className="text-xl">No location data found</div>;
 
-  return <div>Map</div>;
+  return (
+    !loading && (
+      <Map
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: 15,
+        }}
+        style={{ width: "100%", height: 500 }}
+        mapStyle={`https://api.maptiler.com/maps/streets-v4/style.json?key=${process.env.NEXT_PUBLIC_MAPTILER_KEY}`}
+      >
+        <Marker longitude={lng} latitude={lat} anchor="bottom">
+          <Image src={pin} alt="location" width={40} height={40} />
+        </Marker>
+      </Map>
+    )
+  );
 };
 
 export default PropertyMap;
