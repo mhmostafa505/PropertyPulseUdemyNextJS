c4cb598a704ee02d37fd09ca9560c6d206f0632b
Mohammad Hossein <kavieswarten@gmail.com>
2026-08-21 12:47:50 +0330

    pagination system added, lightbox in property details page added to images, featured part in home page added completely

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,10 +1,17 @@
 import PropertyCard from "@/components/PropertyCard";
+import Pagination from "@/components/Pagination";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 
-const PropertiesPage = async () => {
+const PropertiesPage = async ({ searchParams }) => {
   await connectDB();
-  const properties = await Property.find({}).lean();
+  const { page = 1, pageSize = 9 } = await searchParams;
+  const skip = (page - 1) * pageSize;
+
+  const total = await Property.countDocuments({});
+  const properties = await Property.find({}).skip(skip).limit(pageSize);
+
+  const showPagination = total > pageSize;
 
   return (
     <section className="px-4 py-6">
@@ -17,6 +24,13 @@ const PropertiesPage = async () => {
               <PropertyCard key={property._id} property={property} />
             ))}
           </div>
+        )}
+        {showPagination && (
+          <Pagination
+            page={parseInt(page)}
+            pageSize={parseInt(pageSize)}
+            totalItems={total}
+          />
         )}
       </div>
     </section>
--- components/FeaturedPropertyCard.jsx
+++ components/FeaturedPropertyCard.jsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 import {
   FaBed,
   FaBath,
@@ -8,7 +8,7 @@ import {
   FaMapMarker,
 } from "react-icons/fa";
 
-const PropertyCard = ({ property }) => {
+const FeaturedPropertyCard = ({ property }) => {
   const getRateDisplay = () => {
     const { rates } = property;
     if (rates.monthly) {
@@ -21,37 +21,32 @@ const PropertyCard = ({ property }) => {
   };
 
   return (
-    <div className="rounded-xl shadow-md relative">
-      <Link href={`/properties/${property._id}`}>
-        <Image
-          src={property.images[0]}
-          alt=""
-          width="0"
-          height="0"
-          sizes="100vw"
-          className="w-full h-auto rounded-t-xl"
-        />
-      </Link>
-      <div className="p-4">
-        <div className="text-left md:text-center lg:text-left mb-6">
-          <div className="text-gray-600">{property.type}</div>
-          <h3 className="text-xl font-bold">{property.name}</h3>
-        </div>
-        <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
+    <div className="bg-white rounded-xl shadow-md relative flex flex-col md:flex-row">
+      <Image
+        src={property.images[0]}
+        alt=""
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-auto rounded-t-xl md:rounded-tr-none md:rounded-l-xl w-full md:w-2/5"
+      />
+      <div className="p-6">
+        <h3 className="text-xl font-bold">{property.name}</h3>
+        <div className="text-gray-600 mb-4">{property.type}</div>
+        <h3 className="absolute top-[10px] left-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
           {getRateDisplay()}
         </h3>
-
         <div className="flex justify-center gap-4 text-gray-500 mb-4">
           <p>
-            <FaBed className="md:hidden lg:inline" /> {property.beds}{" "}
+            <FaBed className="inline-block mr-2" /> {property.beds}{" "}
             <span className="md:hidden lg:inline">Beds</span>
           </p>
           <p>
-            <FaBath className="md:hidden lg:inline" /> {property.baths}{" "}
+            <FaBath className="inline-block mr-2" /> {property.baths}{" "}
             <span className="md:hidden lg:inline">Baths</span>
           </p>
           <p>
-            <FaRulerCombined className="md:hidden lg:inline" />
+            <FaRulerCombined className="inline-block mr-2" />{" "}
             {property.square_feet}{" "}
             <span className="md:hidden lg:inline">sqft</span>
           </p>
@@ -60,24 +55,24 @@ const PropertyCard = ({ property }) => {
         <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
           {property.rates.nightly && (
             <p>
-              <FaMoneyBill className="md:hidden lg:inline" /> Nightly
+              <FaMoneyBill className="inline mr-2" /> Nightly
             </p>
           )}
           {property.rates.weekly && (
             <p>
-              <FaMoneyBill className="md:hidden lg:inline" /> Weekly
+              <FaMoneyBill className="inline mr-2" /> Weekly
             </p>
           )}
           {property.rates.monthly && (
             <p>
-              <FaMoneyBill className="md:hidden lg:inline" /> monthly
+              <FaMoneyBill className="inline mr-2" /> Monthly
             </p>
           )}
         </div>
 
-        <div className="border border-gray-100 mb-5"></div>
+        <div className="border border-gray-200 mb-5"></div>
 
-        <div className="flex flex-col lg:flex-row justify-between mb-4">
+        <div className="flex flex-col lg:flex-row justify-between">
           <div className="flex align-middle gap-2 mb-4 lg:mb-0">
             <FaMapMarker className="text-orange-700 mt-1" />
             <span className="text-orange-700">
@@ -97,4 +92,4 @@ const PropertyCard = ({ property }) => {
   );
 };
 
-export default PropertyCard;
+export default FeaturedPropertyCard;
